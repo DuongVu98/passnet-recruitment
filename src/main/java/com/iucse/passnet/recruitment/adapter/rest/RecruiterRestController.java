32b195ead97cbeae47f38ce757b126e2b0ac1fe9
Tung Duong <duongvutung1998@gmail.com>
2020-09-20 22:10:53 +0700

    apply response entity

--- src/main/java/com/iucse/passnet/recruitment/adapter/rest/RecruiterRestController.java
+++ src/main/java/com/iucse/passnet/recruitment/adapter/rest/RecruiterRestController.java
@@ -4,9 +4,11 @@ import com.iucse.passnet.recruitment.adapter.controllers.RecruiterController;
 import com.iucse.passnet.recruitment.domain.dto.Job;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/recruiter")
@@ -24,6 +26,23 @@ public class RecruiterRestController {
     @GetMapping(value = "/posted-jobs")
     public List<Job> getAllPostedJobs() {
         return this.recruiterController.getAllPostedJobs();
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/posted-jobs-test")
+    public ResponseEntity<?> getAllPostedJobsTest() {
+        try {
+            Optional<List<Job>> opt = Optional.ofNullable(this.recruiterController.getAllPostedJobs());
+            //                return notFound();
+            if(opt.isPresent()){
+                return ResponseEntity.ok(opt.get());
+            }else {
+                return null;
+            }
+        } catch (Exception exception) {
+//            return badRequest()
+            return null;
+        }
     }
 
     @PostMapping(value = "/post-job/{teacherId}")
