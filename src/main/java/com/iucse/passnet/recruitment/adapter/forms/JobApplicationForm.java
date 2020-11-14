a434f251f8b224c7a1add2ed8ea3903f760164b7
Tung Duong <duongvutung1998@gmail.com>
2020-11-14 11:03:17 +0700

    experiment with validation

--- src/main/java/com/iucse/passnet/recruitment/adapter/forms/JobApplicationForm.java
+++ src/main/java/com/iucse/passnet/recruitment/adapter/forms/JobApplicationForm.java
@@ -4,10 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 
+import javax.validation.constraints.Min;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class JobApplicationForm {
+    @Min(value = 10, message = "letter should not be less than 10")
     private String letter;
+    @Min(value = 20, message = "content should not less than 20")
     private String content;
 }
