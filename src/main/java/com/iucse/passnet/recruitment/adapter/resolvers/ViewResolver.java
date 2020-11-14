aa74d2a537dec9180836f2b7595f3f45d65cf93b
Tung Duong <duongvutung1998@gmail.com>
2020-11-14 21:01:20 +0700

    test some query

--- src/main/java/com/iucse/passnet/recruitment/adapter/resolvers/ViewResolver.java
+++ src/main/java/com/iucse/passnet/recruitment/adapter/resolvers/ViewResolver.java
@@ -1,15 +1,25 @@
-package com.iucse.passnet.recruitment.usecase.resolvers;
+package com.iucse.passnet.recruitment.adapter.resolvers;
 
 import com.iucse.passnet.recruitment.domain.views.JobApplicationView;
 import com.iucse.passnet.recruitment.domain.views.JobView;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ViewResolver implements GraphQLQueryResolver {
 
+    
+
     public JobView jobView(String id) {
+        JobApplicationView jobApplicationView = JobApplicationView.builder()
+           .content("hello teacher")
+           .letter("hello teacher again")
+           .build();
         return JobView.builder()
+           .id(id)
+           .jobApplications(List.of(jobApplicationView))
            .jobTitle("TA for OOP")
            .build();
     }
