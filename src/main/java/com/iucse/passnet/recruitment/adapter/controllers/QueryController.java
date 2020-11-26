734e78410abc6ec904a614f039972c2053bcd90a
Tony <duongvutung1998@gmail.com>
2020-11-27 00:32:52 +0700

    add view resolver

--- src/main/java/com/iucse/passnet/recruitment/adapter/controllers/QueryController.java
+++ src/main/java/com/iucse/passnet/recruitment/adapter/controllers/QueryController.java
@@ -1,5 +1,6 @@
 package com.iucse.passnet.recruitment.adapter.controllers;
 
+import com.iucse.passnet.recruitment.adapter.resolver.ViewResolver;
 import com.iucse.passnet.recruitment.domain.viewrepos.JobViewRepository;
 import com.iucse.passnet.recruitment.domain.views.JobView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,20 @@ import org.springframework.web.client.HttpClientErrorException;
 @Component
 public class QueryController {
 
+    private final JobViewRepository jobViewRepository;
+    private final ViewResolver viewResolver;
+
     @Autowired
-    private JobViewRepository jobViewRepository;
+    public QueryController(JobViewRepository jobViewRepository, ViewResolver viewResolver) {
+        this.jobViewRepository = jobViewRepository;
+        this.viewResolver = viewResolver;
+    }
 
     public JobView getJobView(String id) {
         if (this.jobViewRepository.findById(id).isPresent()) {
             return this.jobViewRepository.findById(id).get();
         }else {
-            return null;
+            return this.viewResolver.toJobView(id);
         }
     }
 }
