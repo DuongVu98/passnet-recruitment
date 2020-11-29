07b337b83447c5038d675e0ecff06079cdf8bdf5
Tony <duongvutung1998@gmail.com>
2020-11-29 18:24:49 +0700

    update job application view

--- src/main/java/com/iucse/passnet/recruitment/usecase/events/handlers/JobApplicationViewUpdateHandler.java
+++ src/main/java/com/iucse/passnet/recruitment/usecase/events/handlers/JobApplicationViewUpdateHandler.java
@@ -1,17 +1,25 @@
 package com.iucse.passnet.recruitment.usecase.events.handlers;
 
 import com.iucse.passnet.recruitment.domain.aggregate.job.entities.Job;
+import com.iucse.passnet.recruitment.domain.aggregate.job.entities.JobApplication;
+import com.iucse.passnet.recruitment.domain.aggregate.job.vos.JobApplicationId;
 import com.iucse.passnet.recruitment.domain.viewrepos.JobApplicationViewRepository;
+import com.iucse.passnet.recruitment.domain.views.JobApplicationView;
 import com.iucse.passnet.recruitment.usecase.events.events.DomainEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Slf4j(topic = "[JobApplicationViewUpdateHandler]")
 public class JobApplicationViewUpdateHandler implements IEventHandler {
 
     private final JobApplicationViewRepository jobApplicationViewRepository;
+
+    private JobApplicationId jobApplicationId;
+    private Job aggregate;
 
     @Autowired
     public JobApplicationViewUpdateHandler(JobApplicationViewRepository jobApplicationViewRepository) {
@@ -23,12 +31,26 @@ public class JobApplicationViewUpdateHandler implements IEventHandler {
         switch (event.getEventTypes()) {
             case TeacherPostedJob:
             case StudentAppliedJob:
-                this.updateFromAggregate(event.getAggregate());
+                this.aggregate = event.getAggregate();
+                this.jobApplicationId = (JobApplicationId) event.getEntityId();
+                this.updateFromAggregate();
+                break;
             case TeacherAcceptedJob:
         }
     }
 
-    private void updateFromAggregate(Job aggregate) {
-        
+    private void updateFromAggregate() {
+        Optional<JobApplication> optional = this.aggregate.getJobApplications().stream().filter(jobApplication -> jobApplication.getId().equal(this.jobApplicationId)).findFirst();
+        if(optional.isPresent()){
+            JobApplication jobApplication = optional.get();
+            JobApplicationView jobApplicationView = JobApplicationView.builder()
+                    .id(this.jobApplicationId.getValue())
+                    .studentId(jobApplication.getApplicationOwner().getValue())
+                    .letter(jobApplication.getLetter().getValue())
+                    .content(jobApplication.getContent().getValue())
+                    .state(jobApplication.getApplicationState().getValue().name())
+                    .build();
+            this.jobApplicationViewRepository.save(jobApplicationView);
+        }
     }
 }
