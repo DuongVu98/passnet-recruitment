0cec450e475e4803b9152d865af77d31849ba757
Tung Duong <duongvutung1998@gmail.com>
2020-12-06 18:11:12 +0700

    format code

--- src/main/java/com/iucse/passnet/recruitment/config/SecurityConfiguration.java
+++ src/main/java/com/iucse/passnet/recruitment/config/SecurityConfiguration.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,16 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-           .cors()
-           .and()
-           .csrf().disable()
-           .authorizeRequests()
-           .antMatchers(HttpMethod.POST, "/**").permitAll()
-           .antMatchers(HttpMethod.GET, "/**").permitAll();
-    }
-
-
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.cors()
+			.and()
+			.csrf()
+			.disable()
+			.authorizeRequests()
+			.antMatchers(HttpMethod.POST, "/**")
+			.permitAll()
+			.antMatchers(HttpMethod.GET, "/**")
+			.permitAll();
+	}
 }
