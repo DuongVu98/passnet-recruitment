47cde0e5978d3bc06af738b45b7df519d0cf031b
DuongVu98 <duongvutung1998@gmail.com>
2021-07-23 09:20:10 +0700

    some refactor

--- src/main/java/com/iucse/passnet/recruitment/domain/forms/JobCreationForm.java
+++ src/main/java/com/iucse/passnet/recruitment/domain/forms/JobCreationForm.java
@@ -1,29 +1,34 @@
 package com.iucse.passnet.recruitment.domain.forms;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class JobCreationForm {
+    @JsonProperty("jobTitle")
     @NotBlank(message = "jobTitle should not be empty")
     private String jobTitle;
 
+    @JsonProperty("courseName")
     @NotBlank(message = "courseName should not be empty")
     private String courseName;
 
+    @JsonProperty("requirement")
     private String requirement;
 
+    @JsonProperty("content")
     private String content;
 
+    @JsonProperty("semester")
     @NotBlank(message = "semester should not be empty")
     private String semester;
 
+    @JsonProperty("organizationId")
     private String organizationId;
 }
